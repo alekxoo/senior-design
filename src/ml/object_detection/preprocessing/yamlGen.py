5fd405264f97244be6c384b81e18f48edf7887f5
Luigi Medrano <luigimedrano03@gmail.com>
2025-02-07 08:20:24 -0600

    updated workflow yaml

--- src/ml/object_detection/preprocessing/yamlGen.py
+++ src/ml/object_detection/preprocessing/yamlGen.py
@@ -27,3 +27,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+'''
+Process for front end:
+
+assuming that the user can upload 1 car at a time
+
+1. User logs into account(we get user's name and unique ID(if needed))
+2. User creates new race (This will be a new folder on their profile)
+3. Create a new Folder (E.g. race-xxxx, with subfolders (e.g images, weights, config))
+3. User uploads batch of photos for Car 1 (these photos would be stored in that race folder under the images subfolder; we will data split later in training)
+4. User inputs label for the car (that is name of the folder of images)
+5. User submits above information for Car 1
+6. yamlGen.py parses submitted information to get num_classes + 1 and add id+ class label for Car 1
+
+(Repeat steps 2-4 for as many cars he wants to upload until finished)
+
+6. yaml file = race-xxxx -> config 
+7. For each class = race-xxxx -> images -> [class_label_created_by_user]
+8. Start training script (should generate train/validation subfolders within race-xxxx -> images)
+9. Generated model weights after training = race-xxxx -> weights
+'''
