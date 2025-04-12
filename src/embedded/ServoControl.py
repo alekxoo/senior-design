d4b399ce81e7b5366181445952351df9a8306ee2
AbdullahAlwakeel <114311494+AbdullahAlwakeel@users.noreply.github.com>
2025-04-12 18:35:25 -0500

    ServoControl

--- src/embedded/ServoControl.py
+++ src/embedded/ServoControl.py
@@ -17,8 +17,8 @@ def vel_y(v):
 
 def set_vel_y(v):
 	global servoReading
-	old_r = f.get(SERVO_ADDR)
-	servoReading = max(65, min(145, old_r + v)) #clamp between 0 and 95
+	#old_r = f.get(SERVO_ADDR)
+	servoReading = max(65, min(145, servoReading + v)) #clamp between 0 and 95
 	f.set(SERVO_ADDR, servoReading, flag=0) #flag=0 means don't wait for response from device
 
 
