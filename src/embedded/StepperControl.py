f7de2ca9fe20bae1ffa0845a1ac42e8e0c7708d0
AbdullahAlwakeel <114311494+AbdullahAlwakeel@users.noreply.github.com>
2025-04-12 17:35:13 -0500

    remove unneded pwm for stepper

--- src/embedded/StepperControl.py
+++ src/embedded/StepperControl.py
@@ -13,42 +13,46 @@ sleep_pin = 32 #TODO find a good pin
 
 gpio.setup(step_pwm_pin, gpio.OUT, initial=gpio.LOW)
 gpio.setup(dir_pin, gpio.OUT, initial=gpio.LOW)
-gpio.setup(sleep_pin, gpio.OUT, initial=gpio.LOW)
+gpio.setup(sleep_pin, gpio.OUT, initial=gpio.LOW) #sleeping
 
 
 step = gpio.PWM(step_pwm_pin, 200) #freq is irrelevant here, since we change it to determine stepping speed
-dir_ = gpio.PWM(dir_pin, 10)
-sleep_ = gpio.PWM(sleep_pin, 10) #comment these out if PWM is not needed
+#dir_ = gpio.PWM(dir_pin, 10)
+#sleep_ = gpio.PWM(sleep_pin, 10) #comment these out if PWM is not needed
 
-dir_.start(100) #duty cycle (100% = always on)
-sleep_.start(100)
+#dir_.start(100) #duty cycle (100% = always on)
+#sleep_.start(100)
 step.start(0)
 
 num_steps = 210 # steps per revolution
 microsteps = 32 #microstep setting (fixed with soldering)
 
 def calc_pwm_freq(hz): #hz: motor revolutions per second
-	return hz * num_steps * microsteps
+	return hz * num_steps * microsteps * 2.0
 
 def set_dir(d):
 	if d:
-		dir_.ChangeDutyCycle(100)
+		gpio.output(dir_pin, gpio.HIGH)
+		#dir_.ChangeDutyCycle(100)
 	else:
-		dir_.ChangeDutyCycle(0)
+		gpio.output(dir_pin, gpio.LOW)
+		#dir_.ChangeDutyCycle(0)
 
 
 #one step per second = minimal speed
 def set_speed(v):
-	if v < (1.0 / num_steps): #minimal speed (Hz)
+	if v < (10.0 / num_steps): #minimal speed (Hz) = 10 steps per second
 		step.ChangeDutyCycle(0)
-		sleep_.ChangeDutyCycle(0) #sleep is active low (0 = sleeping, 1 = not sleeping)
+		#sleep_.ChangeDutyCycle(0) #sleep is active low (0 = sleeping, 1 = not sleeping)
+		gpio.output(sleep_pin, gpio.LOW) #sleep
 	else:
-		sleep_.ChangeDutyCycle(100)
+		#sleep_.ChangeDutyCycle(100)
+		gpio.output(sleep_pin, gpio.HIGH) #no sleep
 		step.ChangeFrequency(calc_pwm_freq(v))
 		step.ChangeDutyCycle(50)
 
 def stepper_shutdown():
-	sleep_.ChangeDutyCycle(0)
+	gpio.output(sleep_pin, gpio.LOW)
 
 
 def vel_x(v):
