738a463765dae798208f609f7f1b8a86460714d2
Luigi Medrano <luigimedrano03@gmail.com>
2025-04-17 17:52:09 -0500

    updates to transform

--- src/ml/object_detection/run/ml_embeddedprod_jetson_gui.py
+++ src/ml/object_detection/run/ml_embeddedprod_jetson_gui.py
@@ -151,7 +151,8 @@ class VehicleTrackerApp:
         
         # Image transformation
         self.transform = transforms.Compose([
-            transforms.Resize((224, 224)),
+            # transforms.Resize((224, 224)),
+            transforms.CenterCrop((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
